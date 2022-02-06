8f979a00e3218198f47c980bdd6889f268f430cc
Lucas Frantz <maxkronus@gmail.com>
2022-02-06 19:17:02 -0300

    filtrar relatorio por datas

--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -8,9 +8,31 @@ defmodule Flightex.Bookings.Report do
     File.write( order_list)
   end
 
+  def generate(filename \\ "report.csv", from_date, to_date) do
+    order_list = build_order_list(from_date, to_date)
+    filename|>
+    File.write( order_list)
+  end
+
+  defp filter_by_date(booking_list, from_date, to_date) do
+    booking_list
+    |> Enum.filter(fn (order) ->
+      order.complete_date >= from_date
+      && order.complete_date <= to_date
+    end
+    )
+  end
+
   defp build_order_list() do
     BookingAgent.list_all()
     |> Map.values()
+    |> Enum.map(fn booking -> booking_string(booking) end)
+    |> IO.inspect()
+  end
+  defp build_order_list(from_date, to_date) do
+    BookingAgent.list_all()
+    |> Map.values()
+    |> filter_by_date(from_date, to_date)
     |> Enum.map(fn booking -> booking_string(booking) end)
     |> IO.inspect()
   end
